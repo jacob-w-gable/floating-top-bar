f7d30572d9f8e1f4c14bdf667e9ff24677b67b34
Jacob Gable <jacob.w.gable+pro@gmail.com>
2021-04-25 22:06:23 -0500

    add transparent, dark, light themes; install script

--- install.sh
+++ install.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 
-glib-compile-schemas src/schemas/
+RUNNING=1
+while [ $RUNNING -eq 1 ]; do
+	echo "How would you like the top bar to look when a window is fullscreen?"
+	echo "1: Transparent"
+	echo "2: Dark"
+	echo "3: Light"
+	read FULLANSWER
+	if [ $FULLANSWER -lt 1 ] || [ $FULLANSWER -gt 3 ]; then
+		echo "Invalid input"
+	else
+		RUNNING=0
+	fi
+done
+if [ $FULLANSWER -eq 1 ]; then
+	echo "Selected transparent top bar"
+fi
+if [ $FULLANSWER -eq 2 ]; then
+	echo "Selected dark top bar"
+fi
+if [ $FULLANSWER -eq 3 ]; then
+	echo "Selected light top bar"
+fi
 
 rm -r ~/.local/share/gnome-shell/extensions/floating-top-bar@jacob-w-gable.github.io
 
@@ -8,4 +29,16 @@ mkdir ~/.local/share/gnome-shell/extensions/floating-top-bar@jacob-w-gable.githu
 
 cp -r src/* ~/.local/share/gnome-shell/extensions/floating-top-bar@jacob-w-gable.github.io
 
+if [ $FULLANSWER -eq 2 ]; then
+	sed -i "2s/.*/background-color: rgba(50, 50, 50, 1.0);/" ~/.local/share/gnome-shell/extensions/floating-top-bar@jacob-w-gable.github.io/stylesheet.css
+	sed -i "3s/.*/transition-duration: 200ms;/" ~/.local/share/gnome-shell/extensions/floating-top-bar@jacob-w-gable.github.io/stylesheet.css
+	sed -i "13s/.*/transition-duration: 200ms;/" ~/.local/share/gnome-shell/extensions/floating-top-bar@jacob-w-gable.github.io/stylesheet.css
+fi
+if [ $FULLANSWER -eq 3 ]; then
+	sed -i "2s/.*/background-color: rgba(230, 230, 230, 1.0);/" ~/.local/share/gnome-shell/extensions/floating-top-bar@jacob-w-gable.github.io/stylesheet.css
+	sed -i "3s/.*/transition-duration: 200ms;/" ~/.local/share/gnome-shell/extensions/floating-top-bar@jacob-w-gable.github.io/stylesheet.css
+	sed -i "13s/.*/transition-duration: 200ms;/" ~/.local/share/gnome-shell/extensions/floating-top-bar@jacob-w-gable.github.io/stylesheet.css
+fi
 
+gnome-extensions disable floating-top-bar@jacob-w-gable.github.io
+gnome-extensions enable floating-top-bar@jacob-w-gable.github.io
